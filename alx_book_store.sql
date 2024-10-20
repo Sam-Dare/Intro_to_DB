b71dda98b01fc6851947bfc968ad4d23c98b4828
Samuel Oluwadare <oluwadaresamuel39@gmail.com>
2024-10-20 23:43:35 +0100

    Correct alx_book_store.sql file third time

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -36,11 +36,11 @@ CREATE TABLE Orders (
 );
 
 -- Order Details Table
-CREATE TABLE IF NOT EXISTS order_details (
-    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE Order_Details (
+    orderdetailid INT PRIMARY KEY AUTO_INCREMENT,
     order_id INT,
     book_id INT,
-    quantity DOUBLE NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES books(book_id)
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
